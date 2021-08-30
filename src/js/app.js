0fc9455a9c34285804b8190a17ac14a6fa040b85
foussedev <89755387+foussedev@users.noreply.github.com>
2021-08-30 17:10:46 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+var text=" Hello, bienvenue sur ma page github. ";
+var test="</br>,Je me nomme : <span class=\"green\">Kaboré Fousséni</span>, </br>,j'ai <span class=\"green\">24 ans Titulaire d'une licence en Macroéconomie Gestion de Développement</span>,</br>,je suis un passioné d'informatique., </br> <strong></br>Compétences :</br></strong></br>, <span class=\"green\">HTML5/CSS3</span>   Niveau : Intermédiaire </br>,<span class=\"green\">PHP/MYSQL</span>    Niveau : Débutant,</br> <strong></br>Contact :</br></strong>,<span>foussedev@gmail.com</span>";
+
 var arrayCitation=" \"Peu import à quel point Tu es dévasté Par ta propre faiblesse ou Ta propre inutilité... Attise la flamme de ton coeur, Serre les dents et va de l'avant. Si tu t'arrête et  que tu t'assoies Par terre, le cours du temps, lui, Ne s'arrêtera pas...\"                 ";
 var auteur="</br>,<span class=\"autor\"> Rengoku Kyojiro</span>,";
 var title="<span > Pensée du jour :</span>, "
